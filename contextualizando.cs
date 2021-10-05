df254a2d52888dc5c51857c4e89ac85af67cfc0c
Sophia de Sousa <68878547+Sophia09-pixel@users.noreply.github.com>
2021-10-05 12:53:42 -0300

    Create contextualizando.cs

---
+++ contextualizando.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u0041';
+            byte n1 = 254;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.6;
+            string nome = "Sophia";
+            object obj1 = "Lucia Sousa";
+            object obj2 = 234.78;
+
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            
+          
+        }
+    }
+}
