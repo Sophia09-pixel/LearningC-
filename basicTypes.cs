6d047a63e78c4299308f1217685eced5686eb247
Sophia de Sousa <68878547+Sophia09-pixel@users.noreply.github.com>
2021-10-05 12:57:34 -0300

    Create basicTypes.cs

---
+++ basicTypes.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
+            decimal n4 = decimal.MaxValue;
+
+
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+
+            
+          
+        }
+    }
+}
